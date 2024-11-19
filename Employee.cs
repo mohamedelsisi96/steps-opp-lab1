2db272a89077b57641e3b52a8a636503b80fbd53
mohamedelsisi96 <mohamed.elsysey@gmail.com>
2024-11-19 04:36:02 +0200

    first commit

---
+++ Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opplab1
+{
+    internal class Employee
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        private int _salary;
+        private string? _phone;
+        public string? Address { get; set; }
+        public int Salary
+        {
+            get { return _salary; }
+            set
+            {
+                if (value < 6000)
+                {
+                    _salary = 6000;
+                }
+                else
+                {
+                    _salary = value;
+                }
+            }
+        }
+        public string? Phone
+        {
+            get { return _phone; }
+            set
+            {
+                if (value != null && value.Length == 11 && (value.StartsWith("010") || value.StartsWith("011") || value.StartsWith("012") || value.StartsWith("015")))
+                {
+                    _phone = value;
+                }
+                else
+                {
+                    Console.WriteLine("please enter valide phone number ");
+                }
+            }
+        }
+        public void IncreaseSalary(int bounes)
+        {
+            _salary += bounes;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"the name of employee is {Name??"Not Set"} \n the Id of employee is {Id } \n the salary of employee is {_salary} \n the phone of employee is {_phone ?? "Not Set"} \n the address of employee is {Address ?? "Not Set"}  ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Opplab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Name = "Omar Mohamed Elsisi";
+            emp.Id = 1;
+            emp.Phone = "01064819492";
+            emp.Salary = 7000;
+            emp.Address = "Cairo";
+            Console.WriteLine(emp.Name);
+            Console.WriteLine(emp.Salary);
+            emp.IncreaseSalary(1200);
+            Console.WriteLine(emp.Salary);
+            emp.Display();
+            Employee[] EmpArr=new Employee[3] ;
+            EmpArr[0] = new Employee();
+            EmpArr[1] = new Employee();
+            EmpArr[2] = new Employee(); EmpArr[0] = new Employee();
+            EmpArr[1] = new Employee();
+            EmpArr[2] = new Employee();
+            EmpArr[0].Name = "ahmed ali";
+            EmpArr[0].Id = 2;
+            EmpArr[0].Phone = "01067130897";
+            EmpArr[0].Salary = 8500;
+            EmpArr[0].Address = "Benha";
+            EmpArr[1].Name = "Mohamed Ehab ";
+            EmpArr[1].Id = 3;
+            EmpArr[1].Phone = "01009642294";
+            EmpArr[1].Salary = 9000;
+            EmpArr[1].Address = "Alex";
+            EmpArr[2].Name = "ebrahim Ehab";
+            EmpArr[2].Id = 4;
+            EmpArr[2].Phone = "01102204916";
+            EmpArr[2].Salary = 10000;
+            EmpArr[2].Address = "Tanta";
+            foreach(Employee empl in EmpArr)
+            {
+                empl.Display();
+            }
+        }
+    }
+}
